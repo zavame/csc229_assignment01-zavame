adcdffe7fa8bd5c5de314f79304aa64d23d81521
zavame <zavame@farmingdale.edu>
2024-02-02 19:28:27 -0500

    Add name data member Add setter and getter to name Add default constructor Add driver class

--- src/main/java/com/mycompany/csc229_assignment01/Course.java
+++ src/main/java/com/mycompany/csc229_assignment01/Course.java
@@ -40,11 +40,42 @@ public class Course {
         return ID;
     }
 
-    public void setID(int ID) {
+    public void setID(int ID, String Name, String Code) {
         this.ID = ID;
+        this.Name = Name;
+        this.Code = Code;
     }
 
     private String name;
     private String code;
 
+}
+public String getName()
+    return Name;
+}
+
+public void setName(String Name) {
+    this.Name = Name;
+}
+
+public String getCode() {
+  return Code;
+}
+
+public void setCode(String Code) {
+    this.Code = Code;
+   }
+}
+
+public class Driver {
+    public static void main(String[] args) {
+        Course course = new Course(25110,"Moaath","CSC229");
+        course.setID(2);
+        course.setName("Moaath");
+        course.setCode("CSC229");
+        System.out.println("ID: "+course.getID());
+        System.out.println("Name: "+course.getName());
+        System.out.println("Code: "+course.getCode());
+
+    }
 }
